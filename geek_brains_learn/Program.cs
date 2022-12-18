fd44c39aeb2c9a78bbe1bd822220c4a0b495d876
Anton <domeniqueflo@gmail.com>
2022-12-18 12:31:27 +0300

    fix 8 day in HollyDay

--- geek_brains_learn/Program.cs
+++ geek_brains_learn/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-       // Console.WriteLine(Lesson2.SecondNumber(345));
-       // Console.WriteLine(Lesson2.ThirdNumber(34));
+        Console.WriteLine(Lesson2.SecondNumber(345));
+        Console.WriteLine(Lesson2.ThirdNumber(34));
         Console.WriteLine(Lesson2.HollyDay(70));
     }
 }
