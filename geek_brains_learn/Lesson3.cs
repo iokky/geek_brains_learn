e64ce4255578d7a9ad8156e8b9850ad830623687
Anton <domeniqueflo@gmail.com>
2022-12-24 12:03:08 +0300

    Leason3

---
+++ geek_brains_learn/Lesson3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace geek_brains_learn
+{
+    public static class Lesson3
+    {
+        public static string Polin(int num)
+        {
+            string stringNum = num.ToString();
+            if (stringNum.Length != 5)
+                return "число не 5 значное";
+
+            return stringNum[..2] == $"{stringNum[4]}{stringNum[3]}" ? "Да" : "Нет";
+        }
+
+        public static double Distance(Vector3 v1, Vector3 v2) => Vector3.Distance(v1, v2);
+
+        public static dynamic Cube(int a) => Enumerable.Range(1, a).ToArray().Select(i => Math.Pow(i, 3)); 
+
+    }
+}
