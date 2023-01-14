8208d2360456ca9a89ae7e948d0c19ebb1a79f0e
iokky <58838881+iokky@users.noreply.github.com>
2023-01-14 11:27:24 +0300

    Leason 4

---
+++ geek_brains_learn/Lesson4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geek_brains_learn;
+
+public static class Lesson4
+{
+   public static int Degree(int a, int b)
+    {
+        var result = 1;
+        for (int i = 1; i <= b; i++)
+        {
+            result = result * a;
+        }
+        return result;
+    }
+
+    public static int Sum(int a)
+    {
+        string data = a.ToString();
+        int result = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            result += int.Parse(data[i].ToString());
+        }
+        return result;
+    }
+
+    public static int[] Array(string data)
+    {
+        var array = data.Split(',');
+        int[] result = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            result[i] = int.Parse(array[i]);
+        }
+        return result;
+    }
+}
--- geek_brains_learn/Program.cs
+++ geek_brains_learn/Program.cs
@@ -5,18 +5,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        /*Console.WriteLine(Lesson2.SecondNumber(345));
-        Console.WriteLine(Lesson2.ThirdNumber(34));
-        Console.WriteLine(Lesson2.HollyDay(70));*/
+        //Console.WriteLine(Lesson4.Degree(3, 5));
+        //Console.WriteLine(Lesson4.Sum(82));
 
-        //Console.WriteLine(Lesson3.Polin(23435));
-        //Console.WriteLine(Lesson3.Distance(new Vector3(3, 6, 8), new Vector3(2, 1, -7)));
-        foreach (var item in Lesson3.Cube(5))
+        var data = Console.ReadLine();
+        var res = Lesson4.Array(data);
+        foreach ( var item in res )
         {
-            Console.WriteLine(item);
+            Console.WriteLine(item);    
         }
-        
-
     }
 
 }
