18e5fd1a850c73a20195eb1592514d6713cd1044
iokky <58838881+iokky@users.noreply.github.com>
2023-01-29 10:44:38 +0300

    Leasson9

---
+++ geek_brains_learn/Leasson9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geek_brains_learn
+{
+    public class Leasson9
+    {
+        private static int result;
+        public static void SequenceNumber(int a)
+        {
+            Console.WriteLine(a);
+            if(a > 1) SequenceNumber(a - 1);
+            return;
+        }
+
+        public static int SumInRange(int m, int n)
+        {
+            if(n > m) 
+            {
+                result += n;
+                n--;
+                SumInRange(m, n);
+            }
+            return result;
+        }
+    }
+}
--- geek_brains_learn/Program.cs
+++ geek_brains_learn/Program.cs
@@ -5,31 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        /*        int[,] array = new int[,]
-                {
-
-                };
-                int[][] data = Lesson8.MultiArraySort(array);
-                foreach (var item in data)
-                {
-                    foreach (var i in item)
-                    {
-                        Console.Write(i);
-                    }
-                    Console.WriteLine();
-                }
-
-
-                var MultiData = Lesson8.MatrixMultiplication();
-                foreach (var item in data)
-                {
-                    Console.WriteLine(item);
-                }*/
-
-        //Console.WriteLine(Lesson8.TrippleArrayGenerator());
-        //Lesson8.TrippleArrayGenerator();
-        Lesson8.SpiralAddedToArray();
-
+        int result = 0;
+        Console.WriteLine(Leasson9.SumInRange(5, 10));
     }
 
 }
